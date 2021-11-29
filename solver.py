0b16ec61209fd01c2fcaf6dce21558d4159e8404
aaron <aaron.zev@berkeley.edu>
2021-11-28 19:45:24 -0800

    done for the day

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 from parse import read_input_file, write_output_file
 import os
 
-def solve(tasks):
+def solve(tasks, seed):
     """
     Args:
         tasks: list[Task], list of igloos to polish
@@ -10,8 +10,8 @@ def solve(tasks):
     """
     
     # HYPERPARAMETERS
-    PPD_WEIGHT = .8
-    DURATION_WEIGHT = .05
+    PPD_WEIGHT = .7
+    DURATION_WEIGHT = .02
     DEADLINE_WEIGHT = 1 - PPD_WEIGHT - DURATION_WEIGHT
     
     priority_func = lambda x: PPD_WEIGHT * x.perfect_benefit / x.duration + DURATION_WEIGHT * (-x.duration) + DEADLINE_WEIGHT * (-x.deadline)
@@ -33,11 +33,16 @@ def solve(tasks):
 
 # Here's an example of how to run your solver.
 if __name__ == '__main__':
-    for input_path in os.listdir('inputs/inputs/small'):
-        if input_path[0] == '.':
-            continue
-        full_input_path = 'inputs/inputs/small/' + input_path
-        output_path = 'outputs/' + input_path[:-3] + '.out'
-        tasks = read_input_file(full_input_path)
-        output = solve(tasks)
-        write_output_file(output_path, output)
+    NUM_TRIALS = 10
+    for trial in NUM_TRIALS:
+        seed = trial
+        for folder in ['small/', 'medium/', 'large/']:
+            os.mkdir("outputs/" + folder)
+            for input_path in os.listdir('inputs/' + folder):
+                if input_path[0] == '.':
+                    continue
+                full_input_path = 'inputs/' + folder + input_path
+                output_path = 'outputs/' + folder + input_path[:-3] + '.out'
+                tasks = read_input_file(full_input_path)
+                output = solve(tasks, trial)
+                write_output_file(output_path, output)
